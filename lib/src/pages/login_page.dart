a1967c0b3ef2984d302534885540a73cbb17b3c0
Carlos Izarra <eduizarra2@hotmail.com>
2023-05-13 12:47:03 -0500

    feat: finish login

--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -11,59 +12,98 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   bool _isTapped = false;
+  bool _obscureText = false;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: const Color.fromRGBO(191, 183, 143, 1),
-      child: Column(children: [
-        Expanded(
-            flex: 5,
-            child: Stack(
-              //fit: StackFit.expand,
-              children: [
-                Container(
-                  decoration: const BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage('assets/images/old-dog.png'),
-                          fit: BoxFit.cover)),
-                  child: BackdropFilter(
-                    filter: ImageFilter.blur(sigmaX: 2.5, sigmaY: 2.5),
-                    child: Center(
-                      child: Card(
-                        elevation: 10,
-                        color: Colors.black.withOpacity(0.5),
-                        child: const SizedBox(
-                          width: 300,
-                          height: 200,
-                          child: Center(
-                            child: Text(
-                              'Logo y BarkBeat',
-                              style: TextStyle(fontSize: 30, color: Colors.white),
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Container(
+        color: const Color.fromRGBO(191, 183, 143, 1),
+        child: Column(children: [
+          Expanded(
+              flex: _isTapped ? 4 : 5,
+              child: Stack(
+                //fit: StackFit.expand,
+                children: [
+                  Container(
+                    decoration: const BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage('assets/images/old-dog.png'),
+                            fit: BoxFit.cover)),
+                    child: BackdropFilter(
+                      filter: ImageFilter.blur(sigmaX: 2.5, sigmaY: 2.5),
+                      child: Center(
+                        child: Card(
+                          elevation: 10,
+                          color: Colors.black.withOpacity(0.5),
+                          child: const SizedBox(
+                            width: 300,
+                            height: 200,
+                            child: Center(
+                              child: Text(
+                                'Logo y BarkBeat',
+                                style: TextStyle(
+                                    fontSize: 30, color: Colors.white),
+                              ),
                             ),
                           ),
                         ),
                       ),
                     ),
                   ),
-                ),
-                //Container(
-                //color: Color.fromRGBO(0, 0, 0, 0.5),
-                //),
-              ],
-            )),
-        Expanded(
+                  //Container(
+                  //color: Color.fromRGBO(0, 0, 0, 0.5),
+                  //),
+                ],
+              )),
+          Expanded(
             flex: 5,
-            child: AnimatedSwitcher(
-                duration: const Duration(milliseconds: 500),
-                transitionBuilder: (Widget child, Animation<double> animation) {
-                  return FadeTransition(
-                  opacity: animation,
-                  child: child,
-                );
-                },
-                child: _isTapped ? _loginInputsWidget() : _presentationWidget())),
-      ]),
+            child: AnimatedContainer(
+              duration: Duration(milliseconds: 500),
+              curve: Curves.easeInOut,
+              width: MediaQuery.of(context).size.width * 2,
+              child: Stack(
+                children: [
+                  AnimatedPositioned(
+                    duration: Duration(milliseconds: 500),
+                    curve: Curves.easeInOut,
+                    left: _isTapped ? 0 : MediaQuery.of(context).size.width,
+                    child: Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: MediaQuery.of(context).size.height / 10 * 5,
+                        color: Colors.blue,
+                        child: _loginInputsWidget()),
+                  ),
+                  AnimatedPositioned(
+                    duration: Duration(milliseconds: 500),
+                    curve: Curves.easeInOut,
+                    left: _isTapped ? -MediaQuery.of(context).size.width : 0,
+                    child: Container(
+                        height: MediaQuery.of(context).size.height / 10 * 5,
+                        width: MediaQuery.of(context).size.width,
+                        //width: MediaQuery.of(context).size.width,
+                        color: Colors.green,
+                        child: _presentationWidget()),
+                  ),
+                ],
+              ),
+            ),
+            /*child: AnimatedSwitcher(
+                  duration: const Duration(milliseconds: 500),
+                  transitionBuilder:
+                      (Widget child, Animation<double> animation) {
+                    return FadeTransition(
+                      opacity: animation,
+                      child: child,
+                    );
+                  },
+                  child: _isTapped
+                      ? _loginInputsWidget()
+                      : _presentationWidget())*/
+          ),
+        ]),
+      ),
     );
   }
 
@@ -153,8 +193,11 @@ class _LoginPageState extends State<LoginPage> {
                         child: Container(
                             color: const Color.fromRGBO(2, 71, 85, 1),
                             child: const Center(
-                              //color: Colors.red,
-                              child: Text(
+                                //color: Colors.red,
+                                child: Icon(Icons.keyboard_arrow_right,
+                                    color: Colors.white, size: 28)
+
+                                /*Text(
                                 ">",
                                 style: TextStyle(
                                     color: Colors.white,
@@ -163,8 +206,8 @@ class _LoginPageState extends State<LoginPage> {
                                     fontSize: 24,
                                     height: 2,
                                     decoration: TextDecoration.none),
-                              ),
-                            )),
+                              ),*/
+                                )),
                       )
                     ]),
                   ),
@@ -177,23 +220,210 @@ class _LoginPageState extends State<LoginPage> {
 
   Widget _loginInputsWidget() {
     return Container(
-        color: const Color.fromRGBO(191, 183, 143, 1),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Material(
-              child: TextFormField(
-              // The validator receives the text that the user has entered.
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter some text';
-                }
-                return null;
-              },
+      color: Color.fromRGBO(191, 183, 143, 1),
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        Container(
+          margin: EdgeInsets.only(left: 10),
+          child: TextButton(
+              style: TextButton.styleFrom(
+                minimumSize: const Size(15, 0),
+              ),
+              onPressed: () => {
+                    setState(() {
+                      _isTapped = !_isTapped;
+                    })
+                  },
+              child: Icon(
+                Icons.keyboard_arrow_left,
+                color: Colors.white,
+              )),
+        ),
+        Material(
+          color: Colors.transparent,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Correo electrónico',
+                      style: TextStyle(
+                          color: Color.fromRGBO(2, 94, 115, 1),
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Europa',
+                          fontSize: 18),
+                    ),
+                    const SizedBox(height: 15),
+                    TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      cursorColor: Color.fromRGBO(2, 94, 115, 1),
+                      style: const TextStyle(
+                        color: Color.fromRGBO(1, 31, 38, 1),
                       ),
+                      onTapOutside: (event) => {
+                        //event.
+                      },
+
+                      decoration: const InputDecoration(
+                        //filled: true,
+
+                        //prefixText: '@sda',
+                        //fillColor: Color.fromARGB(0, 0, 0, 0),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Color.fromRGBO(2, 94, 115, 1), width: 2.0),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.blueGrey, width: 2.0),
+                        ),
+                        hintText: 'email@example.com',
+                        hintStyle: TextStyle(
+                          color: Colors.grey,
+                        ),
+                        //counterText: "${_email.length.toString()}/40",
+                        counterStyle: const TextStyle(color: Colors.white),
+                        focusColor: Colors.white,
+                        hoverColor: Colors.white,
+                      ),
+                      // The validator receives the text that the user has entered.
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        return null;
+                      },
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Contraseña',
+                      style: TextStyle(
+                          color: Color.fromRGBO(2, 94, 115, 1),
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Europa',
+                          fontSize: 18),
+                    ),
+                    const SizedBox(height: 15),
+                    TextFormField(
+                      keyboardType: TextInputType.text,
+                      cursorColor: Color.fromRGBO(2, 94, 115, 1),
+                      style: const TextStyle(
+                        color: Color.fromRGBO(1, 31, 38, 1),
+                      ),
+                      onTapOutside: (event) => {
+                        //event.
+                      },
+                      obscureText: !_obscureText,
+
+                      decoration: InputDecoration(
+                        //filled: true,
+                        suffixIcon: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              _obscureText = !_obscureText;
+                            });
+                          },
+                          child: Icon(
+                              _obscureText
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                              color: Colors.blueGrey),
+                        ),
+                        //prefixText: '@sda',
+                        //fillColor: Color.fromARGB(0, 0, 0, 0),
+                        enabledBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Color.fromRGBO(2, 94, 115, 1), width: 2.0),
+                        ),
+                        focusedBorder: const OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.blueGrey, width: 2.0),
+                        ),
+                        hintText: '*****',
+                        hintStyle: const TextStyle(
+                          color: Colors.grey,
+                        ),
+                        //counterText: "${_email.length.toString()}/40",
+                        counterStyle: const TextStyle(color: Colors.white),
+                        focusColor: Colors.white,
+                        hoverColor: Colors.white,
+                      ),
+                      // The validator receives the text that the user has entered.
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        return null;
+                      },
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 35,
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                      minimumSize: const Size.fromHeight(50),
+                      backgroundColor: const Color.fromRGBO(2, 71, 85, 1),
+                      textStyle: const TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'Riftsoft',
+                          fontWeight: FontWeight.w500,
+                          fontSize: 18,
+                          letterSpacing: 2.0)),
+                  onPressed: () {},
+                  child: const Text('Iniciar sesión'),
+                ),
+                /*const SizedBox(
+                  height: 20,
+                ),
+                Center(
+                  child: Text(
+                    '¿Olvidaste tu contraseña?',
+                    style: TextStyle(
+                        fontFamily: 'Europa',
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ),*/
+                const SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                    '¿No tienes una cuenta? ',
+                    style: TextStyle(
+                        fontFamily: 'Europa',
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500),
+                  ),
+                  Text(
+                    'Registrate',
+                    style: TextStyle(
+                        fontFamily: 'Europa',
+                        color: Color.fromRGBO(2, 94, 115, 1),
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ],)
+              ],
             ),
-          ]
           ),
+        ),
+      ]),
     );
   }
 }
